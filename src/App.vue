d3e865c63e924c9a35ea37dfc6fab94c096d4b36
ELVIS1230 <vladimirortiz1230@gmail.com>
2024-11-21 23:33:32 -0500

    Componente card con animacion

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,26 @@
 <template>
   <div class=" h-screen m-0">
     <div class="h-screen flex">
-      <div class="mt-44 ml-48">
-        <p class="text-5xl">Hello web,</p>
-        <h1 class="font-extrabold text-[175px]">I'm Elvis</h1>
-        <div class="flex gap-96 w-11/12 pr-10">
-          <p class="w-1/3">
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a
-            galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but
-            also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s
-            with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing
-            software like Aldus PageMaker including versions of Lorem Ipsum.
-          </p>
-          <div class="flex flex-col mt-16 gap-4">
-            <p class="text-xl">Aquí va un texto para explicar algo</p>
-            <p class="text-xl">Aquí otro texto va un texto y aquí también</p>
-
+      <div class="mt-44 grid grid-cols-2">
+        <div class="ml-36 border-2 border-red-500 w-auto">
+          <p class="text-5xl">Hello web,</p>
+          <h1 class="font-extrabold text-[175px]">I'm Elvis</h1>
+          <div class="flex gap-96  pr-10">
+            <p class="w-2/3">
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took
+              a
+              galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but
+              also the leap into electronic typesetting, remaining essentially unchanged. It was popularized in the
+              1960s
+              with the release of Legraste sheets containing Lorem Ipsum passages, and more recently with desktop
+              publishing
+              software like Lauds PageMaker including versions of Lorem Ipsum.
+            </p>
           </div>
+        </div>
+        <div class="flex items-center justify-center gap-4 border-2 border-blue-500">
+          <CardProfile />
         </div>
       </div>
       <div class="w-64 vertical-text flex gap-20 flex-col  justify-center">
@@ -28,7 +31,13 @@
     </div>
   </div>
 </template>
+<script lang="ts">
+import CardProfile from './components/CardProfile.vue';
 
+export default {
+  components: { CardProfile }
+}
+</script>
 <style scoped>
 .text {
   writing-mode: vertical-rl;
---
+++ src/components/CardProfile.vue
@@ -0,0 +1,89 @@
+<template>
+  <section>
+
+    <div class="card">
+      <!-- <p>testoo</p> -->
+      <!-- <img src="/person.jpeg" class="rounded-3xl absolute -z-10"> -->
+      <div class="flex gap-4 absolute bottom-4 left-4 ">
+        <p class="bg-white p-1">Github</p>
+        <p class="bg-white p-1">Github</p>
+        <p class="bg-white p-1">Github</p>
+      </div>
+    </div>
+  </section>
+
+</template>
+
+<script>
+export default {
+  name: 'CardProfile',
+}
+</script>
+
+<style scoped>
+* {
+  --color: red;
+}
+
+.card {
+  --color: #ff88a6;
+  --border-size: 1rem;
+  --bg-color: #5c5a5a;
+  position: relative;
+  background-color: var(--bg-color);
+  width: 380px;
+  height: 427px;
+  margin: 5rem;
+  border-radius: 1.5rem;
+  display: grid;
+  place-content: center;
+  padding: 3rem 2rem;
+  isolation: isolate;
+  overflow: hidden;
+}
+
+.card::before,
+.card::after {
+  content: '';
+  position: absolute;
+}
+
+.card::before {
+  width: 200%;
+  height: 200%;
+  background-image: conic-gradient(#0dff79 0deg,
+      transparent 60deg,
+      transparent 180deg,
+      #0dff79 180deg,
+      transparent 240deg);
+  inset: -50%;
+  z-index: -2;
+  animation: border 8s linear infinite;
+}
+
+@keyframes border {
+  to {
+    transform: rotateZ(-360deg);
+  }
+
+}
+
+.card::after {
+  --inset: 0.37rem;
+  background-image: url('/person.jpeg');
+  background-size: cover;
+  inset: var(--inset);
+  border: 1px solid black;
+  border-radius: 1.2rem;
+  z-index: -1;
+  transition: all 0.35s linear;
+}
+
+.profile-image {
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+  object-fit: cover;
+  margin-bottom: 16px;
+}
+</style>
